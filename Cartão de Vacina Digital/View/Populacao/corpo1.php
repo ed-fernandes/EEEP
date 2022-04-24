6ce31e5fa4781271a311767d13214c679f389ab7
Eduardo Fernandes <104321731+ed-fernandes@users.noreply.github.com>
2022-04-24 16:49:05 -0300

    Add files via upload

---
+++ Cartão de Vacina Digital/View/Populacao/corpo1.php
@@ -0,0 +1,13 @@
+
+<!--<div class="col-12 ">
+  <div class="row">
+    <div class="animate-in-down ">
+        <img class="img-fluid" src="../imagens/in2.png">
+      </div>
+    </div>
+  </div>-->
+
+<section class="corpo1">
+    
+</section>
+
---
+++ Cartão de Vacina Digital/View/Populacao/informacoes.php
@@ -0,0 +1,20 @@
+<div class="py-5" id="informacoes">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 animate-in-left">
+          <h2 class="text-primary text-center">Posto de Saúde X</h2>
+
+          <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d994.5853069504396!2d-39.30770321189382!3d-4.346915262554508!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7bf07711a348095%3A0xfc31fc0bb3abd963!2sPSF+-+CONJUNTO+HABITACIONAL!5e0!3m2!1spt-PT!2sbr!4v1542460827465' width='700' height='300' frameborder='0' style='border:0' allowfullscreen></iframe>
+        </div>
+
+
+        <div class="col-md-6 align-self-center animate-in-left">
+          <div class="">
+            <img class="img-fluid" src="https://lh5.googleusercontent.com/p/AF1QipME_rBXbozaP2YepDgDpKi2PvyCRRi_QcI5iWZf=w408-h306-k-no">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
