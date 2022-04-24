2066c7250c40db0f81fe66b173daa16fdb8ac6b6
Eduardo Fernandes <104321731+ed-fernandes@users.noreply.github.com>
2022-04-24 17:34:15 -0300

    Delete Cartão de Vacina Digital directory

--- Cartão de Vacina Digital/Model/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header("Location: ../View/Publico/index.php");
-?>
--- Cartão de Vacina Digital/View/Agente/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$nomeBd = "bd_cadastro";
-
-$conexao = mysqli_connect($servidor,$usuario, $senha, $nomeBd );
- ?>
